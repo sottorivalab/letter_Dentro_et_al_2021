243af51b637ab6e9b5e2e5e8a3385f5ecbe3b0c9
Salvatore Milite <militesalvatore@gmail.com>
2021-12-06 12:56:33 +0100

    Update germline_analysis.R

--- germline_analysis.R
+++ germline_analysis.R
@@ -317,20 +317,20 @@ Tarone_Boostrap <-
 # Meta can be downloaded from consensus.20170217.purity.ploidy.txt.gz
 # SNPs need to be extracted from closed access BAM files, they need to have the following columns: ["chr", "ref", "alt", "AD", "DP"].
 
-cnas <-  readRDS("letter_cnas.rds")
+#cnas <-  readRDS("letter_cnas.rds")
 
-snvs <-  readRDS("letter_snvs.rds")
+#snvs <-  readRDS("letter_snvs.rds")
 
-snps <-  readRDS("letter_snps.rds")
+#snps <-  readRDS("letter_snps.rds")
 
-meta <- readRDS("letter_metadata.rds")
+#meta <- readRDS("letter_metadata.rds")
 
 
-res <-  mapply(snps, snvs, cnas,names(cnas), meta, 
+#res <-  mapply(snps, snvs, cnas,names(cnas), meta, 
                            FUN = function(x,y,z,n,m) 
                              try(generate_report_overdispersion(x,y,z,n, m)),
                            SIMPLIFY = F)
 
-res %>% saveRDS("germline_analysis.rds")
+#res %>% saveRDS("germline_analysis.rds")
 
 
