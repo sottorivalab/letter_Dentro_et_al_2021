ee650cf704f0beeaf4c7a3db75584c2d91544bc3
Salvatore Milite <militesalvatore@gmail.com>
2021-12-17 16:15:52 +0100

    added 2 articles

--- example_simulated/run_pyclone_sim.sh
+++
@@ -1,8 +0,0 @@
-source ~/miniforge3/etc/profile.d/conda.sh
-
-conda activate pyclone-vi
-
-
-pyclone-vi fit -i simulation_input.tsv -o simulation.h5 -c 40 -d beta-binomial -r 10
-
-pyclone-vi write-results-file -i simulation.h5 -o simulation.tsv
